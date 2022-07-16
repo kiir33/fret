f94c14ac7280e62d3e8292197d19fb7edd91497a
Kiran Banmala <kiranbanmala3@gmail.com>
2022-07-16 07:10:23 +0545

    Updated know us description

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Intro = () => {
   return (
     <div className="row p-4">
       <p className="h4 text-center fw-bolder my-4">ABOUT FRET</p>
-      <div className="mx-auto my-2">   
+      <div className="mx-auto my-2">
         <p>
-          Fret is an instrumental trio from Nepal, creating new form of music, furnishing 
-          fusion grooves in an evolved manner blending traditional and ethnic music with an 
-          influence of eastern classical, western music and world music through Sarangi, Flute 
-          and Newari drums. 
-          Fret Nepal.
-         </p>
+          Fret is an instrumental trio from Nepal, creating new forms of music,
+          furnishing fusion grooves in an evolved manner blending traditional
+          and ethnic music with an influence of eastern classical, western music
+          and world music through Sarangi, Flute and Newari drums. The trio’s
+          vision is to explore their instruments in an innovative way and
+          exhibit it in their style. The trio constitutes three individual
+          artists from diverse musical backgrounds combined together to form a
+          distinct world music genre.
+        </p>
       </div>
       {/* <div className="mx-auto my-2 col-md-12 col-lg-4">
         <img src={cover} className="img-fluid rounded" />
       </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/KnowUs.js
+++ src/components/KnowUs.js
@@ -30,29 +30,42 @@ export default KnowUs;
 export const names = ["Yajoo Acharya", "Prabin Lakhaju", "Roshik Jadhari"];
 export const images = [yajoo, prabin, roshik];
 export const details = [
-  "Yajoo is a young enthusiast flutist from Bhaktapur who has completed her graduation in Music, \
-  flute as a major from Rabindra Bharati University, Kolkata, India. She was a winner of 23rd \
-  Bal Sastriya Sangeet competition held by Kirateswor Sangeet Ashram 0n 2016.  She used to play \
-  as a band member of Gorkhali Girls Band. She has been actively participating in many musical \
-  events. She has been learning flute from her very early age and moving on with the wide view \
-  of contributing through her music.",
+  "Yajoo is an ardent flutist from Bhaktapur with graduation in Music majoring in\
+  Flute from Rabindra Bharati University, Kolkata, India. Being an active\
+  learner and performer since her childhood, she has performed in both National\
+  and International music events. She has been actively learning North Indian\
+  classical Music, particularly flute and vocal music. She was a winner of 23rd\
+  Bal Shastriya Sangeet competition held by Kirateswor Sangeet Ashram 0n 2016.\
+  She used to play as a band member of Gorkhali Girls Band and many other bands\
+  too. She is working as a teacher in Bhaktapur as well as Kathmandu so that she\
+  could bring some change in the teaching mechanism of Nepal. She has\
+  collaborated and shared the stage with many artists from Kathmandu and\
+  Bhaktapur. She has been learning flute since childhood and strives to\
+  contribute through her music.",
 
-  "Prabin Lakhaju, an enthusiast percussionist from Bhaktapur  who has been learning and \
-  performing music since his childhood is engaged with newari Dapha traditions and now \
-  studying Ethnomusicology in Kathmandu University, Department of Music.  He is an active \
-  practitioner of Dapha Traditions. Later on, after joining the Department of Music, Kathmandu \
-  University, he has been learning North Indian Classical Music especially vocal, tabla and \
-  Navabaja ensemble. \
-  He has been involved in project Folklok as a researcher. He is working as a music teacher \
-  and as an ethnomusicologist in his locality and reviving music which has been lost in the \
-  past few years. He has collaborated with different artists and musicians at Bhaktapur in \
-  different events. He also had been involved in Theatre shows as a background musician at \
-  Aastha Theatre, Bhaktapur and Kunja Natak Ghar, New Baneshwor.",
+  "Prabin Lakhaju, an enthusiastic percussionist from Bhaktapur who has been\
+  learning and performing music since his childhood. He is engaged with Newari\
+  Dapha traditions and is studying Ethnomusicology in Kathmandu University,\
+  Department of Music. He is an active practitioner of Dapha Traditions.\
+  Following his enrollment to the Department of Music, Kathmandu University, he\
+  has been learning North Indian Classical Music particularly Vocal, Tabala and\
+  Nava - baja ensemble of Bhaktapur. He has been involved in project Folklok as\
+  a researcher. He is working as a music teacher and as an ethnomusicologist in\
+  his locality and reviving music which has not been in practice in the past few\
+  years. He has collaborated with different artists and musicians from Bhaktapur\
+  in different events. He also had been involved in Theatre shows as a\
+  background musician at Aastha Theatre, Bhaktapur and Kunja Natak Ghar, New\
+  Baneshwor. He is recently working at Nepal Music Archive",
 
-  "Roshik Jadhari is a young artist from Bhaktapur who has been playing different musical \
-  instrument from his early childhood. He used to play in Band Khalimalicha, and teaching \
-  music in Mukti Sangeet Prasikshyan Kendra, Bhaktapur.  He is studying Ethnomusicology in \
-  Kathmandu University, Department of music. Beside Sarangi, he has been learning and performing \
-  flute and newari instruments in different places. He has been playing vital role to preserve \
-  the endangered Mwali Baja.",
+  "Roshik Jadhari is a young enthusiast musician from Bhaktapur who strives to\
+  explore the unique traditional and folk instruments of Nepal. He has been\
+  learning music since his childhood and has been participating in musical\
+  events since then. He is also an active Dapha Practitioner with a\
+  determination to revitalise the instruments which are in stage of extinction.\
+  He has been teaching music in Mukti Sangeet Prasikshyan Kendra, Bhaktapur. He\
+  is studying Ethnomusicology in Kathmandu University, Department of music.\
+  Beside Sarangi, he has been learning and performing flute and Newari\
+  instruments. He has shared the stage with many well known musicians from\
+  Kathmandu and Bhaktapur. He has been playing a vital role to preserve the\
+  endangered Mwali Baja.",
 ];
