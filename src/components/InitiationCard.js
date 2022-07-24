baedec503ecbda2d22286f38c56d9c14556ad144
Kiran Banmala <kiranbanmala3@gmail.com>
2022-07-24 09:59:16 +0545

    Typo fixes

--- src/components/InitiationCard.js
+++ src/components/InitiationCard.js
@@ -1,16 +1,11 @@
 import React from "react";
-import Truncate from "react-truncate";
 const InitiationCard = ({ image, title, details }) => {
   return (
     <div className="my-4 p-0">
-      <p className="card-title h5 mb-2">{title}</p>
-      <div className="text-start">
-        <img
-          className="img-initiations rounded-2"
-          src={image}
-          alt="Image"
-        />
-        <p className="text-justify fw-light">{details}</p>
+      <p className="h5 mx-2">{title}</p>
+      <div className="text-justify ps-2">
+        <img className="img-initiations rounded-2" src={image} alt="Image" />
+        <p className="fw-light">{details}</p>
       </div>
     </div>
   );
--- src/components/OurInitiations.js
+++ src/components/OurInitiations.js
@@ -8,13 +8,13 @@ const details = [
   <p>
     Bhaktapur is well known all over the world for its culture, heritage and
     traditions. Within the culture of Bhaktapur, Musical culture and tradition
-    is an important accept. Music is a very essential factor which keeps the
+    is an important aspect. Music is a very essential factor which keeps the
     ambience alive in Bhaktapur. WIthout the existing traditional music,
     Bhaktapur will lose its soul. So we have named this initiation as ‘Ambience
     of Bhaktapur’. Nowadays, we can see our young generation are getting
     detached from their own origin, of which music is a dominant aspect. To keep
-    the ambience of Bhaktapur alive, music should be understood and learned.
-    So,in order to understand anything, we have to spread knowledge about it. So
+    the ambience of Bhaktapur alive, music should be understood and learned. So,
+    in order to understand anything, we have to spread knowledge about it. So
     the most effective way of doing so is to give it an academic direction.
     ‘Ambience of Bhaktapur’ is an initiation designed by Fret with a motive to
     make the music and musical instrument known among students from their school
